45188cb7c676a0abb9bfd29f29d1edfea2d664ae
Victor Akihito Kamada Tomita <victor.kamada.tomita@gmail.com>
2020-09-23 16:25:56 -0300

    "hot fix"

--- Top Down/Assets/Scripts/PlayerMovement.cs
+++ Top Down/Assets/Scripts/PlayerMovement.cs
@@ -69,6 +69,12 @@ public class PlayerMovement : MonoBehaviour
         
         movement.x = Input.GetAxisRaw("Horizontal");
         movement.y = Input.GetAxisRaw("Vertical");
+
+        
+
+        
+
+
         endOfAiming = Input.GetButtonUp("Fire1");
         mousePos = cam.ScreenToWorldPoint(Input.mousePosition);
     
