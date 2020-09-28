b4a780cd5546dd14cdaaf35e8c3a9b9fa8bf0f89
Victor Akihito Kamada Tomita <victor.kamada.tomita@gmail.com>
2020-09-27 21:04:58 -0300

    Update bullet.cs

--- Top Down/Assets/Scripts/bullet.cs
+++ Top Down/Assets/Scripts/bullet.cs
@@ -4,7 +4,6 @@ using UnityEngine;
 
 public class bullet : MonoBehaviour
 {
-  
     // se a bala entrar em contato com algo ela é destruida.
     void OnCollisionEnter2D(Collision2D  other) {
         Destroy(gameObject);
