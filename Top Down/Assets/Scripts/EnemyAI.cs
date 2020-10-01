e1243b3c8c531870d01b3006ba083856ad6e8280
Victor Akihito Kamada Tomita <victor.kamada.tomita@gmail.com>
2020-10-01 20:16:56 -0300

    checkInsight working

--- Top Down/Assets/Scripts/EnemyAI.cs
+++ Top Down/Assets/Scripts/EnemyAI.cs
@@ -8,11 +8,40 @@ public class EnemyAI : MonoBehaviour
     // inimigo
     GameObject enemy;
 
+    [Space]
+    [Header("player")]
     // alvo
     public Transform target;
+    private Transform player;
+   
 
+    
+    [Space]
+    [Header("Patroling")]
+    public float timeChangePoint = 2f; 
+    public float walkPointRange = 5f;
+    public float lastTime = 0f;
+
+
+    [Space]
+    [Header("Attacking")]
+    public float timeBetweenAttacks;
+    bool alreadyAttacked;
+
+
+    [Space]
+    [Header("States")]
+    public float sightRange = 10;
+    public float attackRange;
+    // verifica o inimigo está persguindo o player
+    public bool onChase = false;
+    public bool playerInSightRange, playerInAttackRange;
+
+
+    [Space]
+    [Header("atributes")]
     // speed
-    public float speed = 200f;
+    public float speed = 5f;
     // proximo ponto de escolha
     public float nextWaypointDistance = 3f;
 
@@ -23,23 +52,43 @@ public class EnemyAI : MonoBehaviour
     Seeker seeker;
     Rigidbody2D rb;
 
+    void Awake() {
+        player = GameObject.Find("player").transform;
+
+        GameObject newEmptyGO = new GameObject();
+        target = newEmptyGO.transform;
+
+        target.position = new Vector2(transform.position.x , transform.position.y);
+    }
+
 
     // Start is called before the first frame update
     void Start()
     {
+        
+
         seeker = GetComponent<Seeker>();
         rb = GetComponent<Rigidbody2D>();
-
-
+        
         InvokeRepeating("UpdatePath", 0f, .5f);
 
     }
 
     void UpdatePath()
     {
+
         if (seeker.IsDone()){
             seeker.StartPath(rb.position, target.position, OnPathComplete);
         }
+    }
+
+    void getRandomPoint()
+    {
+        /* gera um ponto aleatório dentro do rangenpoint*/
+        target.position = new Vector2 (
+                    transform.position.x + Random.Range(-walkPointRange, walkPointRange),
+                    transform.position.y + Random.Range(-walkPointRange, walkPointRange)
+        );
     }
 
     void OnPathComplete(Path p )
@@ -51,10 +100,28 @@ public class EnemyAI : MonoBehaviour
         }
 
     }
+    
+    bool checkIfInSight(){
+        /* verificando se está na área de alcance de visão do inimigo */
+        // transform.LookAt(player);
+        float distance = Vector2.Distance(transform.position, player.position);
+        playerInSightRange = distance <= sightRange;
+    
+        if (playerInSightRange)
+        {
+            onChase = true;
+            target = player;
+        }
+        return playerInSightRange;
+    }
+
 
     // Update is called once per frame
     void FixedUpdate()
     {
+        if(target == null)
+            return;
+
         if(path == null)
             return;
         if ( currentWaypoint >= path.vectorPath.Count){
@@ -85,5 +152,20 @@ public class EnemyAI : MonoBehaviour
         {
             transform.localScale = new Vector3(1f,1f,1f);
         }
+        
+        if (!onChase)
+        {
+            checkIfInSight();
+        }
+
+        /* verificando tempo de troca pontos para se andar (para fazer patroling) */
+        if (Time.time - lastTime >= timeChangePoint && !onChase)
+        {
+            getRandomPoint();
+            lastTime = Time.time;
+        }
+        
+
+        
     }
 }
