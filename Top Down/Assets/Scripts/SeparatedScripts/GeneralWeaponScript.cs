7a463426f957d38ef2aa7fa9512b2cfb8fc5ef71
lucasebling <lucasebling@bol.com.br>
2020-10-03 18:37:28 -0300

    Versão mais Molecular
    Versão com scripts separados feito de forma que, se deus quiser, a criação/implementação de outras coisas seja mais fácil e flexível.

---
+++ Top Down/Assets/Scripts/SeparatedScripts/GeneralWeaponScript.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GeneralWeaponScript : MonoBehaviour
+{
+    //Neste Script fazemos todos os controles que qualquer arma terá
+    //Tais controles incluem: Mira, Relações de Munição, armas equipadas.
+    //Este Script estará ligado a um objeto chamdo "holster", filho do player
+    //Tal objeto exsiste para facilitar a troca de armas e coisas assim.
+    
+    //Gameobject da arma
+    //Na versão final provavelmente teremos uma lista
+    //que começara vazia, e vamos instanciando objetos para dentro dela.
+    public GameObject weapon;
+
+    //Sprite da Arma, tiramos do gameobject da arma
+    SpriteRenderer wpimg;
+
+    //basicamente this.gameObject
+    GameObject holster;
+
+    //Vetor de posição do mouse, usado na mira.
+    public Vector3 camvec;
+
+    void Start()
+    {
+        //Ao iniciar pegamos o sprite da arma e o holster.
+        holster = this.gameObject;
+        wpimg = weapon.GetComponent<SpriteRenderer>();
+            }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //definimos o angulo que vamos usar para a mira.
+        AimAngle();
+
+        //Se estamos atirando, vamos atirar
+        if(Input.GetButtonUp("Fire1")){
+            //Atirar será chamado da arma em si
+            //Desse modo, desse handler podemos atirar com todas as armas
+            //Parte de lidar com munição ainda não criada.
+            //Esta função é herdada da classe Gun - Mas como chamar "genericamente" ainda não ficou
+            //Claro pra mim, estou vendo como será.
+            weapon.GetComponent<Pistol>().Shoot(camvec);
+        }
+
+    }
+
+    public void AimAngle(){
+        //pegamos o angulo da arma.
+        camvec = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        camvec.z = 0;
+        Vector3 auxcamvec = (camvec - transform.position).normalized;
+        camvec = auxcamvec;
+        //Angles from -90 to 90 are on normal position
+        //Other angles are inverted
+        float angle = Mathf.Atan2(auxcamvec.y,auxcamvec.x) * Mathf.Rad2Deg;
+        bool invert = false;
+        if(!(angle <= 90 && angle >= -90)){
+            invert = true;
+        }
+
+        //Basicamente vemos se damos o flipzinho ou não.
+        /*if(invert){
+           wpimg.flipY = invert; 
+           holster.transform.eulerAngles = new Vector3(holster.transform.eulerAngles.x,holster.transform.eulerAngles.y,angle); 
+        }
+        else{
+            wpimg.flipY = invert; 
+            holster.transform.eulerAngles = new Vector3(holster.transform.eulerAngles.x,holster.transform.eulerAngles.y,angle); 
+        }*/
+        wpimg.flipY = invert; 
+        holster.transform.eulerAngles = new Vector3(holster.transform.eulerAngles.x,holster.transform.eulerAngles.y,angle);
+    }
+
+
+}
---
+++ Top Down/Assets/Scripts/SeparatedScripts/Pistol.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pistol : Gun
+{
+    // Start is called before the first frame update
+    
+
+    GameObject self;
+    public GameObject bullet;
+
+    public float bulletspeed;
+
+    GameObject tip;
+
+    //Ao criar uma pistola, setamos então sua munição, e armas;
+    void Awake(){
+        clipsize = 6;
+        ammo = 0;
+        curammo = 6;
+
+        self = this.gameObject;
+
+        //self.GetComponent<SpriteRenderer>().sprite = GetSprite("Pistol");  
+        //Tip = Ponta da arma - Usada para determinar a posição de onde sai o tiro.
+        tip = self.transform.Find("Tip").gameObject;  
+    }
+    
+    void Start()
+    {
+        clipsize = 6;
+        ammo = 0;
+        curammo = 6;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public override int Shoot(Vector3 aimvec){
+       //Primeiro checamos a questão da munição
+       if(curammo == 0){
+           return 0;
+       }
+       //se temos munição, atiramos; Tiro de pistóla é basico
+       else{
+           curammo--;
+           GameObject nbullet = Instantiate(bullet,tip.transform.position,transform.rotation);
+           Rigidbody2D rbb = nbullet.GetComponent<Rigidbody2D>();
+           rbb.AddForce(aimvec * bulletspeed, ForceMode2D.Impulse);
+           Destroy(nbullet, 2.0f);
+       }
+       
+       return 1;
+    }
+
+    //Se foi possivel recarregar = 1
+    //Se não = 0
+    public override int Reload(){
+        //se não tivermos municão guardada
+        if(ammo <= 0){
+            return 0;
+        }
+        //se temos, mas é menor q o tamanho do clipe;
+        else if(ammo < clipsize){
+            curammo = ammo;
+            ammo = 0;
+            return 1;
+        }
+        //se não, recarregamos normalmente
+        else{
+            curammo = clipsize;
+            ammo = ammo - clipsize;
+            return 1;
+        }
+
+    }
+
+    //Sera usada com pacotes de munição.
+    //Podemos estabelecer um limite máximo de munição.
+    public override void Refill(int Ammount)
+    {
+        ammo = ammo + Ammount;
+    }
+}
