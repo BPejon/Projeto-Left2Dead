fdef8b300a6b9b5b12cc5d2ad81f87b3e276ef21
Victor Akihito Kamada Tomita <victor.kamada.tomita@gmail.com>
2020-11-06 16:26:37 -0300

    get your shit back

--- Top Down/Assets/Scripts/bullet.cs
+++ Top Down/Assets/Scripts/bullet.cs
@@ -6,8 +6,10 @@ public class bullet : MonoBehaviour
 {
     // se a bala entrar em contato com algo ela é destruida.
     void OnCollisionEnter2D(Collision2D  other) {
-        if (!other.gameObject.CompareTag("playerBullet")){
+        if (!other.gameObject.CompareTag("playerBullet") 
+            && !other.gameObject.CompareTag("player") ){
             Destroy(gameObject);
         }
+      
     }
 }
--- Top Down/Assets/Scripts/playermelee.cs
+++ Top Down/Assets/Scripts/playermelee.cs
@@ -6,24 +6,75 @@ public class playermelee : MonoBehaviour
 {
     
 
-  
-    public float new_size; 
+    [Space]
+    [Header("melee")]
+    public float newHitSize;
+    public float ForceBack;
+    public float TimeAttacking;
+    public float TimeBTWAttackig;
     public CapsuleCollider2D m_collider;
+    public GameObject bulletPrefab; 
+    
+    
+    float TimeStart; 
+    float TimeEnd;
+    public bool isAttackingMelee;
     Vector2 inicial_size;
+
+
+
     
     // Start is called before the first frame update
     void Start()
     {
-        inicial_size = m_collider.size;
+        TimeStart = -10;
+        TimeEnd = -10;
+        isAttackingMelee = false;
+        inicial_size = new Vector2 (m_collider.size.x, m_collider.size.y);
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown("e")){
-            m_collider.size = (new Vector2(inicial_size.x,inicial_size.y)) * new_size;
+
+        if(Input.GetKeyDown("e") && !isAttackingMelee && Time.time - TimeEnd >= TimeBTWAttackig){
+            isAttackingMelee = true;
+            TimeStart = Time.time;
+            m_collider.size = (new Vector2(inicial_size.x,inicial_size.y)) * newHitSize;
         }
     }
     
+    void FixedUpdate()
+    {
+        if(isAttackingMelee && Time.time - TimeStart > TimeAttacking){
+            isAttackingMelee = false;
+            TimeEnd = Time.time;
+            m_collider.size = (new Vector2(inicial_size.x,inicial_size.y));
+        }
+
+
+    }
+
+    // se a bala entrar em contato com algo ela é destruida.
+    void OnCollisionEnter2D(Collision2D  other) {
+        if (isAttackingMelee)
+        {
+            
+            
+            if (other.gameObject.CompareTag("enemyBullet")){
+                
+                Transform enemyBulletT = other.gameObject.GetComponent<Transform>();
+                Debug.Log("instanciando balas");
+
+                GameObject bullet = Instantiate(bulletPrefab, enemyBulletT.position, enemyBulletT.rotation);
+                Rigidbody2D rbb = bullet.GetComponent<Rigidbody2D>();
+                // adiciona uma força que define a movimentaçao da bala
+                rbb.AddForce(enemyBulletT.up * (-ForceBack), ForceMode2D.Impulse);
+                // depois de 2 segundos o projétil é destruido
+                Destroy(bullet, 8.0f);
+            }
+        }
+        
+    }
 
 }
