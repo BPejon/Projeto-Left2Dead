510fb0c20cc2bb69149dfbe7ca31b9458a973835
BPejon <38378927+BPejon@users.noreply.github.com>
2020-11-23 20:17:16 -0300

    Update SoundManager.cs

--- Top Down/Assets/Scripts/GameHandler/SoundManager.cs
+++ Top Down/Assets/Scripts/GameHandler/SoundManager.cs
@@ -4,19 +4,60 @@ using UnityEngine;
 
 public static class SoundManager{ 
 
+
+    //Para usar o script adicione SoundManager.PlaySound(SoundManager.Sound."Som",local do som);
     public enum Sound{
         pistolShot,
         pistolReload,
         shotgunShot,
         shotgunReload,
-    }
-    public static void PlaySound(Sound sound) {
-        GameObject soundGameObject = new GameObject("Sound");
-        AudioSource audioSource = soundGameObject.AddComponent<AudioSource>();
-       audioSource.PlayOneShot(GetAudioClip(sound));
+        playerMove,
     }
 
-    
+   
+
+    //Dicionário para analisar casos especificos de tempo para reproduzir um som
+    private static Dictionary<Sound, float> soundTimerDictionary;
+
+    public static void Initialize()
+    {
+        soundTimerDictionary = new Dictionary<Sound, float>();
+        soundTimerDictionary[Sound.playerMove] = 0f;
+    }
+
+    public static void PlaySound(Sound sound) {
+        if (CanPlaySound(sound) == true){
+            GameObject soundGameObject = new GameObject("Sound");
+            AudioSource audioSource = soundGameObject.AddComponent<AudioSource>();
+            audioSource.PlayOneShot(GetAudioClip(sound));
+        }
+    }
+
+    //Função que analisa se o som reproduzira de forma alternativa
+    private static bool CanPlaySound(Sound sound)
+    {
+        float playerMoveTimeMax = 0.05f; 
+        switch (sound){
+            default:
+                return true;
+
+            case Sound.playerMove:
+                if (soundTimerDictionary.ContainsKey(sound)){
+                    float lastTimePlayed = soundTimerDictionary[sound];
+                    if (lastTimePlayed + playerMoveTimeMax < Time.time){
+                        soundTimerDictionary[sound] = Time.time;
+                        return true;
+                    }
+                    else
+                    {
+                        return false;
+                    }
+                }
+                else{
+                    return false;
+                }         
+        }
+    }
     //Função que pega o audio que queremos executar
     private static AudioClip GetAudioClip(Sound sound) {
         foreach (GameAssets.SoundAudioClip soundAudioClip in GameAssets.Instance.soundAudioClipArray){
