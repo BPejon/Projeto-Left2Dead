086d7d0755f8a792bac86f94f28fab8197fdefe0
BPejon <38378927+BPejon@users.noreply.github.com>
2020-12-04 00:58:32 -0300

    Mapas adicionados
    Adicionados laboratorio e jardim.
    Não adicionei mask nem A*

--- Top Down/Assets/Scripts/Shake.cs
+++ Top Down/Assets/Scripts/Shake.cs
@@ -5,12 +5,36 @@ using MilkShake;
 public class Shake : MonoBehaviour
 {
     public Shaker MyShaker;
-    public ShakePreset shake;
-    void Update()
+    public ShakePreset ShakePreset;
+    //The saved shake instance we will be modifying
+    private ShakeInstance myShakeInstance;
+
+    private void Start()
     {
-        if ()
-        {
-            MyShaker.Shake(shake);
-        }
+        //The Shake method returns a Shake Instance that you can save and modify.
+        myShakeInstance = Shaker.ShakeAll(ShakePreset);
+    }
+
+    private void Update()
+    {
+        //Start the shake, with a fade-in time of 1 second.
+        if (Input.GetKeyDown(KeyCode.Q))
+            myShakeInstance.Start(1f);
+
+        //Stop the shake, with a fade-out time of 1 second. Also make sure the shake is not removed by the Shaker once it is fully stopped.
+        if (Input.GetKeyDown(KeyCode.E))
+            myShakeInstance.Stop(1f, false);
+
+        //Pause or unpause the shake, with a fade-out time of 1 second. Pausing will stop the shake from moving, but keep the offset position and rotation.
+        if (Input.GetKeyDown(KeyCode.P))
+            myShakeInstance.TogglePaused(1f);
+
+        //Increase the shake strength when holding the Plus key on the numpad.
+        if (Input.GetKey(KeyCode.KeypadPlus))
+            myShakeInstance.StrengthScale += Time.deltaTime;
+
+        //Decrease the shake strength when holding the Minus key on the numpad.
+        if (Input.GetKey(KeyCode.KeypadMinus))
+            myShakeInstance.StrengthScale -= Time.deltaTime;
     }
 }
