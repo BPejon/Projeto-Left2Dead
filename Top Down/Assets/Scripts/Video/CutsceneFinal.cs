349eb01caf1cd759d176d8968ede25e2a3a4fd49
BPejon <38378927+BPejon@users.noreply.github.com>
2020-12-11 12:46:35 -0300

    Cutscenes

---
+++ Top Down/Assets/Scripts/Video/CutsceneFinal.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.Video;
+
+public class CutsceneFinal : MonoBehaviour { 
+        public VideoPlayer video;
+
+    void Awake()
+    {
+        video = GetComponent<VideoPlayer>();
+        video.Play();
+        video.loopPointReached += CheckOver;
+
+
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape) || Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            SceneManager.LoadScene(0);//the scene that you want to load after the video has ended.
+        }
+    }
+    void CheckOver(UnityEngine.Video.VideoPlayer vp)
+    {
+        SceneManager.LoadScene(0);//the scene that you want to load after the video has ended.
+    }
+}
